1615918ef3f6d182132a22f4d780d3a5bcc02efb
nguyễn văn thành <46096171+nnguyenvanthanh113@users.noreply.github.com>
2020-05-12 22:02:45 +0700

    khởi tạo project
    add packages bt1,bt2,bt3

---
+++ src/bt1/BT1Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bt1;
+
+/**
+ *
+ * @author ThanhMickey
+ */
+public class BT1Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
